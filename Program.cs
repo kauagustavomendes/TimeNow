6a6eff7a9a37225616f1fa431b19c7de464a5eb0
kanntari <kauagustavomendes@gmail.com>
2024-12-08 18:10:48 -0300

    Primeiro Commit relogio

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class MyDateTime {
+    private DateTime agora = DateTime.Now;
+    public int hh {get; set;}
+    public int mm {get; set;}
+    public int ss {get; set;}
+
+    public int DD {get; set;}
+    public string MM {get; set;}
+    public int YYYY {get; set;}
+
+    public MyDateTime(){
+        hh = agora.Hour;
+        mm = agora.Minute;
+        ss = agora.Second;
+        
+        DD = agora.Day;
+        MM = agora.ToString("MMMM");
+        YYYY = agora.Year;
+    }
+
+    public override string ToString(){
+        return $"{DD}/{MM}/{YYYY}  {hh}:{mm}:{ss}";
+    }
+}
+class App {
+    static void Main(string[] args){
+        MyDateTime Now = new MyDateTime();
+        Console.WriteLine(Now);
+    }
+}
